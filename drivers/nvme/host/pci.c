a7b9f2ad8f445036fbd1bb7557f747caaed51518
shyjumon <shyjumon.n@intel.com>
2018-05-05 01:29:02 +0530

    nvme-pci: SIMICs bug fix
    The KSL-R Simics modeled controller doesn't handle
    Identify value others than 0 or 1, so handling it in driver
    similar to a Qemu emulated controller.
    
    The changes shall be reverted back once SIMICs fix the issue.
    
    Signed-off-by: shyjumon <shyjumon.n@intel.com>
    Signed-off-by: fli24 <fei.li@intel.com>

--- drivers/nvme/host/pci.c
+++ drivers/nvme/host/pci.c
@@ -3150,6 +3150,7 @@ static const struct pci_error_handlers nvme_err_handler = {
 static const struct pci_device_id nvme_id_table[] = {
 	{ PCI_VDEVICE(INTEL, 0x0953),	/* Intel 750/P3500/P3600/P3700 */
 		.driver_data = NVME_QUIRK_STRIPE_SIZE |
+				NVME_QUIRK_IDENTIFY_CNS |	/* FIXME: For Intel Simics */
 				NVME_QUIRK_DEALLOCATE_ZEROES, },
 	{ PCI_VDEVICE(INTEL, 0x0a53),	/* Intel P3520 */
 		.driver_data = NVME_QUIRK_STRIPE_SIZE |
