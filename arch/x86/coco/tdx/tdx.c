d17194fc2b7f849f589644f2167e100cd690a11f
Sean Christopherson <sean.j.christopherson@intel.com>
2019-07-15 11:22:33 -0700

    x86/tdx: Add #VE tracepoint
    Add tracepoint for tracing TDX guest #VE exceptions. It will dump
    RIP, exit reason, exit qual, GPA, instruction length and instruction
    info for each #VE exception occurred.
    
    Also, make trace points RCU idle safe to avoid warnings when RCU
    debugging is enabled.
    
    Reviewed-by: Andi Kleen <ak@linux.intel.com>
    Reviewed-by: Tony Luck <tony.luck@intel.com>
    Signed-off-by: Sean Christopherson <seanjc@google.com>
    Signed-off-by: Kuppuswamy Sathyanarayanan <sathyanarayanan.kuppuswamy@linux.intel.com>

--- arch/x86/coco/tdx/tdx.c
+++ arch/x86/coco/tdx/tdx.c
@@ -12,6 +12,9 @@
 #include <asm/insn-eval.h>
 #include <asm/pgtable.h>
 
+#define CREATE_TRACE_POINTS
+#include <asm/trace/tdx.h>
+
 /* TDX module Call Leaf IDs */
 #define TDX_GET_INFO			1
 #define TDX_GET_VEINFO			3
@@ -501,6 +504,11 @@ static bool virt_exception_user(struct pt_regs *regs, struct ve_info *ve)
 /* Handle the kernel #VE */
 static bool virt_exception_kernel(struct pt_regs *regs, struct ve_info *ve)
 {
+
+	trace_tdx_virtualization_exception_rcuidle(regs->ip, ve->exit_reason,
+			ve->exit_qual, ve->gpa, ve->instr_len, ve->instr_info,
+			regs->cx, regs->ax, regs->dx);
+
 	switch (ve->exit_reason) {
 	case EXIT_REASON_HLT:
 		return handle_halt();
