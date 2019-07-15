623215052005a2ee9ec6de5a146aaf0dd7604fdb
Intel Next Project <intel-next-maintainers@eclists.intel.com>
2019-07-15 12:45:07 -0700

    eywa: squash old commits
    Signed-off-by: Intel Next Project <intel-next-maintainers@eclists.intel.com>

--- init/version.c
+++ init/version.c
@@ -7,6 +7,7 @@
  *  May be freely distributed as part of Linux.
  */
 
+#include <linux/eywa.h>
 #include <generated/compile.h>
 #include <linux/build-salt.h>
 #include <linux/elfnote-lto.h>
@@ -38,7 +39,7 @@ EXPORT_SYMBOL_GPL(init_uts_ns);
 /* FIXED STRINGS! Don't touch! */
 const char linux_banner[] =
 	"Linux version " UTS_RELEASE " (" LINUX_COMPILE_BY "@"
-	LINUX_COMPILE_HOST ") (" LINUX_COMPILER ") " UTS_VERSION "\n";
+	LINUX_COMPILE_HOST ") (" LINUX_COMPILER ") " UTS_VERSION "\n" EYWA_WARNING;
 
 const char linux_proc_banner[] =
 	"%s version %s"
