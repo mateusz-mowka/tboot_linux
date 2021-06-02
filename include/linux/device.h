8014950cbf1f25ac40f7dd93865ec4ec9980edf8
Dave Jiang <dave.jiang@intel.com>
2021-06-02 13:39:42 -0700

    driver core: Add per device pasid param
    Interrupt Message Store (IMS) entry is defined with pasid and and pasid
    enable fields. Add a pasid parameter for 'struct device' so that data
    can be programmed by the irq-ims-msi irqchip as auxdata when allocating IMS
    interrupts. The discussion can be found here [1].
    
    [1]: https://lore.kernel.org/kvm/162164243591.261970.3439987543338120797.stgit@djiang5-desk3.ch.intel.com/T/#m807e8d3850ff2cab040992742fbf4fe58bf48b94
    
    NOT FOR UPSTREAM!!!
    
    Suggested-by: Thomas Gleixner <tglx@linutronix.de>
    Signed-off-by: Dave Jiang <dave.jiang@intel.com>

--- include/linux/device.h
+++ include/linux/device.h
@@ -632,6 +632,7 @@ struct device {
 	void	(*release)(struct device *dev);
 	struct iommu_group	*iommu_group;
 	struct dev_iommu	*iommu;
+	u32			pasid;
 
 	struct device_physical_location *physical_location;
 
@@ -1104,5 +1105,15 @@ extern long sysfs_deprecated;
 #else
 #define sysfs_deprecated 0
 #endif
+
+static inline u32 dev_get_pasid(struct device *dev)
+{
+	return dev->pasid;
+}
+
+static inline void dev_set_pasid(struct device *dev, u32 pasid)
+{
+	dev->pasid = pasid;
+}
 
 #endif /* _DEVICE_H_ */
