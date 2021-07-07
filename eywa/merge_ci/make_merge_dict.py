a91d67c9fbb5c251057b9ed2b6c9a3293c8d8515
Kyle Pelton <kyle.d.pelton@intel.com>
2021-07-07 16:02:56 -0700

    make_merge_dict.py: Use JSON manifest/skip stuck at ref
    Don't track branches where we have stuck_at_ref set. Use manifest.json instead of manifest.
    
    Signed-off-by: Kyle Pelton <kyle.d.pelton@intel.com>

--- eywa/merge_ci/make_merge_dict.py
+++ eywa/merge_ci/make_merge_dict.py
@@ -1,10 +1,9 @@
-#!/usr/bin/env python
+#!/usr/bin/python3
 #simple script to generate json file with all branches and repos out of manifest.in
-#skip internal intel-next repo 
 import json
 
 #file location for blacklist 
-manifest_loc = "eywa/manifest"
+manifest_loc = "eywa/manifest.json"
 
 def add_branch(branches,repo,branch,project):
     for entry in branches:
@@ -13,18 +12,18 @@ def add_branch(branches,repo,branch,project):
             return
     branches.append({"repourl":repo,"branch":[branch],"project_name":project})
 
-def get_branches(manifest_str):
+def get_branches(manifest):
     branches = [] 
     project_base_name="topic_intel_next"
-    #skip linux next kernel repo because that is tracked elsewhere
+    #we don't want buildbot to trigger on eywa or upstream linux changes
     eywa_branch = r"ssh://git@gitlab.devtools.intel.com:29418/intel-next/intel-next-kernel.git"
-    upstream_repo=r"git://git.kernel.org/pub/scm/linux/kernel/git/torvalds/linux.git"
+    upstream_repo=r"https://git.kernel.org/pub/scm/linux/kernel/git/torvalds/linux.git"
     blacklist =[eywa_branch,upstream_repo]
-    for line in manifest_str.split("\n"):
-        if line.split(" ")[0] not in blacklist and len(line) > 0 and line[0] !="#":
-            parsed = line.split()
-            repo = parsed[0]
-            branch = parsed[1]
+    for item in manifest["topic_branches"]:
+        branch = item["branch"]
+        repo = item["repourl"]
+        #Don't track branches where stuck at ref is set
+        if repo not in blacklist and item["stuck_at_ref"] == "" :
             branch_name = branch.replace("/","_")
             branch_name = branch_name.replace(".","_")
             project = repo.split("://")[1]
@@ -36,8 +35,8 @@ def get_branches(manifest_str):
     return branches
 
 def main():
-    manifest = open(manifest_loc).read()
-    print(json.dumps(get_branches(manifest),sort_keys=True,indent=4))
+    bb_manifest = json.load(open(manifest_loc))
+    print(json.dumps(get_branches(bb_manifest),sort_keys=True,indent=4))
 
 if __name__== "__main__":
     main()
