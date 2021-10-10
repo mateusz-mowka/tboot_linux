c472ad9978e5ab0595f0e11f5ec87a4809f98886
Fenghua Yu <fenghua.yu@intel.com>
2021-10-10 23:04:53 +0000

    x86/resctrl: Fix __init issue in rdt_get_on_l3_config()
    Signed-off-by: Fenghua Yu <fenghua.yu@intel.com>

--- arch/x86/kernel/cpu/resctrl/internal.h
+++ arch/x86/kernel/cpu/resctrl/internal.h
@@ -527,7 +527,7 @@ int closids_supported(void);
 void closid_free(int closid);
 int alloc_rmid(void);
 void free_rmid(u32 rmid);
-int rdt_get_mon_l3_config(struct rdt_resource *r);
+int __init rdt_get_mon_l3_config(struct rdt_resource *r);
 void mon_event_count(void *info);
 int rdtgroup_mondata_show(struct seq_file *m, void *arg);
 void rmdir_mondata_subdir_allrdtgrp(struct rdt_resource *r,
