50ef6beb9fa0a8a7a21c214dc52c30760315c67b
Cathy Zhang <cathy.zhang@intel.com>
2021-10-14 14:19:28 +0800

    x86/kvm: Change kvm_set_guest_paused external
    For SGX CPUSVN update is a slow operation, it might cause soft lockup
    happens in KVM guest which is paused when update is running. It
    requires to leverage kvm_set_guest_paused() to avoid such situation.
    
    Make kvm_set_guest_paused external to allow it be used by more
    procedures, like SGX CPUSVN update.
    
    Signed-off-by: Cathy Zhang <cathy.zhang@intel.com>
    Signed-off-by: Qian Wen <qian.wen@linux.intel.com>

--- arch/x86/include/asm/kvm_host.h
+++ arch/x86/include/asm/kvm_host.h
@@ -2156,6 +2156,8 @@ static inline int kvm_cpu_get_apicid(int mps_cpu)
 #endif
 }
 
+extern int kvm_set_guest_paused(struct kvm_vcpu *vcpu);
+
 int memslot_rmap_alloc(struct kvm_memory_slot *slot, unsigned long npages);
 
 #define KVM_CLOCK_VALID_FLAGS						\
