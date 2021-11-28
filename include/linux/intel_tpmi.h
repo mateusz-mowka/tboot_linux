831a5cbed469643689dbcf51087953c6b787b820
Srinivas Pandruvada <srinivas.pandruvada@linux.intel.com>
2021-11-28 07:06:46 -0800

    platform/x86/intel/tpmi: External interface for in kernel modules
    This is only for internal users.
    
    Signed-off-by: Srinivas Pandruvada <srinivas.pandruvada@linux.intel.com>

--- include/linux/intel_tpmi.h
+++ include/linux/intel_tpmi.h
@@ -30,4 +30,9 @@ int intel_tpmi_readq(struct auxiliary_device *auxdev, const void __iomem *addr, 
 int intel_tpmi_writeq(struct auxiliary_device *auxdev, u64 value, void __iomem *addr);
 int tpmi_get_feature_status(struct auxiliary_device *auxdev,
 			    int feature_id, int *locked, int *disabled);
+
+/* In kernel interface only */
+int tpmi_get_info(int package_id, int tpmi_id, int *num_entries, int *entry_size);
+void __iomem *tpmi_get_mem(int package_id, int tpmi_id, int *size);
+void tpmi_free_mem(void __iomem *mem);
 #endif
