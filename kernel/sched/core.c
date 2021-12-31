14e9eb748cd73ea455ad37def133ff3f75187569
Ricardo Neri <ricardo.neri-calderon@linux.intel.com>
2021-12-30 17:27:52 -0800

    sched/x86: Initialize the class of a new task
    Initialize the classification of a task when forked.
    
    Signed-off-by: Ricardo Neri <ricardo.neri-calderon@linux.intel.com>

--- kernel/sched/core.c
+++ kernel/sched/core.c
@@ -4301,6 +4301,11 @@ static void __sched_fork(unsigned long clone_flags, struct task_struct *p)
 	p->se.prev_sum_exec_runtime	= 0;
 	p->se.nr_migrations		= 0;
 	p->se.vruntime			= 0;
+#ifdef CONFIG_SCHED_TASK_CLASSES
+	p->class			= TASK_CLASS_UNCLASSIFIED;
+	p->class_candidate		= TASK_CLASS_UNCLASSIFIED;
+	p->class_debounce_counter	= 0;
+#endif
 	INIT_LIST_HEAD(&p->se.group_node);
 
 #ifdef CONFIG_FAIR_GROUP_SCHED
