8226ee6e7cf6f9753423b53fcc11d09803fd35bc
Zhang Rui <rui.zhang@intel.com>
2022-01-21 12:56:40 +0800

    tools/power turbostat: internal: add support for METEORLAKE processor
    add support for METEORLAKE processor
    
    Signed-off-by: Zhang Rui <rui.zhang@intel.com>

--- tools/power/x86/turbostat/turbostat.c
+++ tools/power/x86/turbostat/turbostat.c
@@ -5362,6 +5362,8 @@ unsigned int intel_model_duplicates(unsigned int model)
 	case INTEL_FAM6_ALDERLAKE:
 	case INTEL_FAM6_ALDERLAKE_L:
 	case INTEL_FAM6_RAPTORLAKE:
+	case INTEL_FAM6_METEORLAKE:
+	case INTEL_FAM6_METEORLAKE_L:
 		return INTEL_FAM6_CANNONLAKE_L;
 
 	case INTEL_FAM6_ATOM_TREMONT_L:
