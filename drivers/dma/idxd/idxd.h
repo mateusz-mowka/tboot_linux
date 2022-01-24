8979a5e99cb1dc9a24b4044dd6b8b130d348148b
Dave Jiang <dave.jiang@intel.com>
2022-01-24 16:10:05 -0700

    dmaengine: idxd: add wq driver name support for accel-config user tool
    With the possibility of multiple wq drivers that can be bound to the wq,
    the user config tool accel-config needs a way to know which wq driver to
    bind to the wq. Introduce per wq driver_name sysfs attribute where the user
    can indicate the driver to be bound to the wq. This allows accel-config to
    just bind to the driver using wq->driver_name.
    
    Signed-off-by: Dave Jiang <dave.jiang@intel.com>

--- drivers/dma/idxd/idxd.h
+++ drivers/dma/idxd/idxd.h
@@ -209,6 +209,7 @@ struct idxd_wq {
 	u64 max_xfer_bytes;
 	u32 max_batch_size;
 	bool ats_dis;
+	char driver_name[WQ_NAME_SIZE + 1];
 };
 
 struct idxd_engine {
@@ -539,6 +540,11 @@ static inline int idxd_wq_refcount(struct idxd_wq *wq)
 {
 	return wq->client_count;
 };
+
+static inline int idxd_wq_driver_name_match(struct idxd_wq *wq, struct device *dev)
+{
+	return (strncmp(wq->driver_name, dev->driver->name, strlen(dev->driver->name)) == 0);
+}
 
 #define MODULE_ALIAS_IDXD_DEVICE(type) MODULE_ALIAS("idxd:t" __stringify(type) "*")
 #define IDXD_DEVICES_MODALIAS_FMT "idxd:t%d"
