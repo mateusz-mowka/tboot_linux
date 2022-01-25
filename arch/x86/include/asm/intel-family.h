e36b40ed5782cc73af5c6c52220385f7c81ef3e5
Tony Luck <tony.luck@intel.com>
2022-01-25 15:17:00 -0800

    x86/cpu: Add Sierra Forest CPU model number
    Signed-off-by: Tony Luck <tony.luck@intel.com>

--- arch/x86/include/asm/intel-family.h
+++ arch/x86/include/asm/intel-family.h
@@ -108,6 +108,8 @@
 
 #define INTEL_FAM6_GRANITERAPIDS_X	0xAD
 
+#define INTEL_FAM6_SIERRAFOREST_X	0xAF
+
 #define INTEL_FAM6_ALDERLAKE		0x97	/* Golden Cove / Gracemont */
 #define INTEL_FAM6_ALDERLAKE_L		0x9A	/* Golden Cove / Gracemont */
 #define INTEL_FAM6_ALDERLAKE_N		0xBE
