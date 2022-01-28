fd778e2b87d9068d3bc90dcf105c92a1cd6f0bd3
Robert Hoo <robert.hu@linux.intel.com>
2022-01-28 12:49:17 +0800

    KVM: x86: LAM: Expose LAM CPUID to user space VMM
    LAM feature is enumerated by (EAX=07H, ECX=01H):EAX.LAM[bit26].
    
    Signed-off-by: Robert Hoo <robert.hu@linux.intel.com>
    Reviewed-by: Jingqi Liu <jingqi.liu@intel.com>

--- arch/x86/kvm/cpuid.c
+++ arch/x86/kvm/cpuid.c
@@ -629,7 +629,7 @@ void kvm_set_cpu_caps(void)
 
 	kvm_cpu_cap_mask(CPUID_7_1_EAX,
 		F(AVX_VNNI) | F(AVX512_BF16) | F(CMPCCXADD) | F(AMX_FP16) |
-		F(AVX_IFMA));
+		F(AVX_IFMA) | F(LAM));
 
 	kvm_cpu_cap_mask(CPUID_7_1_EDX,
 		F(AVX_VNNI_INT8) | F(AVX_NE_CONVERT) | F(PREFETCHIT_0_1)
