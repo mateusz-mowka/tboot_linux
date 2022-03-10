e87712826b384e5c7549b5f7267c79c15ab2d557
Tom Zanussi <tom.zanussi@linux.intel.com>
2022-03-10 10:23:45 -0800

    crypto: iaa - Add per-cpu workqueue table with rebalancing
    The iaa compression/decompression algorithms in later patches need a
    way to retrieve an appropriate IAA workqueue depending on how close
    the associated IAA device is to the current cpu.
    
    For this purpose, add a per-cpu array of workqueues such that an
    appropriate workqueue can be retrieved by simply accessing the per-cpu
    array.
    
    Whenever a new workqueue is bound to or unbound from the iaa_crypto
    driver, the available workqueues are 'rebalanced' such that work
    submitted from a particular CPU is given to the most appropriate
    workqueue available.  There currently isn't any way for the user to
    tweak the way this is done internally - if necessary, knobs can be
    added later for that purpose.  Current best practice is to configure
    and bind at least one workqueue for each IAA device, but as long as
    there is at least one workqueue configured and bound to any IAA device
    in the system, the iaa_crypto driver will work, albeit most likely not
    as efficiently.
    
    Signed-off-by: Tom Zanussi <tom.zanussi@linux.intel.com>

--- drivers/crypto/iaa/iaa_crypto_main.c
+++ drivers/crypto/iaa/iaa_crypto_main.c
@@ -23,8 +23,50 @@
 /* number of iaa instances probed */
 static unsigned int nr_iaa;
 
+static unsigned int nr_cpus;
+static unsigned int nr_nodes;
+
+/* Number of physical cpus sharing each iaa instance */
+static unsigned int cpus_per_iaa;
+
+/* Per-cpu lookup table for balanced wqs */
+static struct idxd_wq * __percpu *wq_table;
+
 static LIST_HEAD(iaa_devices);
 static DEFINE_MUTEX(iaa_devices_lock);
+
+/*
+ * Given a cpu, find the closest IAA instance.  The idea is to try to
+ * choose the most appropriate IAA instance for a caller and spread
+ * available workqueues around to clients.
+ */
+static inline int cpu_to_iaa(int cpu)
+{
+	const struct cpumask *node_cpus;
+	int node, n_cpus = 0, test_cpu, iaa = 0;
+	int nr_iaa_per_node;
+
+	nr_iaa_per_node = nr_iaa / nr_nodes;
+
+	for_each_online_node(node) {
+		node_cpus = cpumask_of_node(node);
+		if (!cpumask_test_cpu(cpu, node_cpus))
+			continue;
+
+		iaa = node * nr_iaa_per_node;
+
+		for_each_cpu(test_cpu, node_cpus) {
+			if (test_cpu == cpu)
+				return iaa;
+
+			n_cpus++;
+			if ((n_cpus % cpus_per_iaa) == 0)
+				iaa++;
+		}
+	}
+
+	return -1;
+}
 
 static struct iaa_device *iaa_device_alloc(void)
 {
@@ -201,10 +243,22 @@ static int save_iaa_wq(struct idxd_wq *wq)
 
 	if (WARN_ON(nr_iaa == 0))
 		return -EINVAL;
+
+	cpus_per_iaa = nr_cpus / nr_iaa;
 out:
 	mutex_unlock(&iaa_devices_lock);
 
 	return 0;
+}
+
+static void clear_wq_table(void)
+{
+	int cpu;
+
+	for (cpu = 0; cpu < nr_cpus; cpu++)
+		*per_cpu_ptr(wq_table, cpu) = NULL;
+
+	pr_debug("cleared wq table\n");
 }
 
 static void remove_iaa_wq(struct idxd_wq *wq)
@@ -215,10 +269,118 @@ static void remove_iaa_wq(struct idxd_wq *wq)
 	list_for_each_entry(iaa_device, &iaa_devices, list) {
 		if (iaa_has_wq(iaa_device, wq)) {
 			del_iaa_wq(iaa_device, wq);
+			if (nr_iaa == 0)
+				clear_wq_table();
 			break;
 		}
 	}
 	mutex_unlock(&iaa_devices_lock);
+
+	if (nr_iaa)
+		cpus_per_iaa = nr_cpus / nr_iaa;
+	else
+		cpus_per_iaa = 0;
+}
+
+static struct idxd_wq *request_iaa_wq(int iaa)
+{
+	struct iaa_device *iaa_device, *found_device = NULL;
+	struct idxd_wq *bkup_wq = NULL, *found_wq = NULL;
+	int cur_iaa = 0, cur_wq = 0, cur_bkup;
+	struct idxd_device *idxd;
+	struct iaa_wq *iaa_wq;
+	struct pci_dev *pdev;
+	struct device *dev;
+
+	mutex_lock(&iaa_devices_lock);
+	list_for_each_entry(iaa_device, &iaa_devices, list) {
+		idxd = iaa_device->idxd;
+		pdev = idxd->pdev;
+		dev = &pdev->dev;
+
+		if (cur_iaa != iaa) {
+			cur_iaa++;
+			continue;
+		}
+
+		found_device = iaa_device;
+		dev_dbg(dev, "getting wq from iaa_device %p (%d)\n",
+			found_device, cur_iaa);
+		break;
+	}
+
+	if (!found_device) {
+		found_device = list_first_entry_or_null(&iaa_devices,
+							struct iaa_device, list);
+		if (!found_device) {
+			pr_debug("couldn't find any iaa devices with wqs!\n");
+			goto out;
+		}
+		cur_iaa = 0;
+
+		idxd = found_device->idxd;
+		pdev = idxd->pdev;
+		dev = &pdev->dev;
+		dev_dbg(dev, "getting wq from only iaa_device %p (%d)\n",
+			found_device, cur_iaa);
+	}
+
+	list_for_each_entry(iaa_wq, &found_device->wqs, list) {
+		/* Prefer unused wq but use if we can't find one */
+		if (idxd_wq_refcount(iaa_wq->wq) > 0) {
+			bkup_wq = iaa_wq->wq;
+			cur_bkup = cur_wq;
+		} else {
+			dev_dbg(dev, "returning unused wq %p (%d) from iaa device %p (%d)\n",
+				iaa_wq->wq, cur_wq, found_device, cur_iaa);
+			found_wq = iaa_wq->wq;
+			goto out;
+		}
+		cur_wq++;
+	}
+
+	if (bkup_wq) {
+		dev_dbg(dev, "returning used wq %p (%d) from iaa device %p (%d)\n",
+			bkup_wq, cur_bkup, found_device, cur_iaa);
+		found_wq = bkup_wq;
+		goto out;
+	}
+out:
+	mutex_unlock(&iaa_devices_lock);
+
+	return found_wq;
+}
+
+static void rebalance_wq_table(void)
+{
+	int node, cpu, iaa;
+	struct idxd_wq *wq;
+
+	if (nr_iaa == 0)
+		return;
+
+	pr_debug("nr_nodes=%d, nr_cpus %d, nr_iaa %d, cpus_per_iaa %d\n",
+		 nr_nodes, nr_cpus, nr_iaa, cpus_per_iaa);
+
+	for (cpu = 0; cpu < nr_cpus; cpu++) {
+		iaa = cpu_to_iaa(cpu);
+		pr_debug("iaa=%d\n", iaa);
+
+		if (WARN_ON(iaa == -1)) {
+			pr_debug("rebalance (cpu_to_iaa(%d)) failed!\n", cpu);
+			return;
+		}
+
+		wq = request_iaa_wq(iaa);
+		if (!wq) {
+			pr_debug("could not get wq for iaa %d!\n", iaa);
+			return;
+		}
+
+		*per_cpu_ptr(wq_table, cpu) = wq;
+		pr_debug("assigned wq for cpu=%d, node=%d = wq %p\n",
+			 cpu, node, wq);
+	}
 }
 
 static int iaa_crypto_probe(struct idxd_dev *idxd_dev)
@@ -258,6 +420,8 @@ static int iaa_crypto_probe(struct idxd_dev *idxd_dev)
 	ret = save_iaa_wq(wq);
 	if (ret)
 		goto err_save;
+
+	rebalance_wq_table();
 out:
 	mutex_unlock(&wq->wq_lock);
 
@@ -280,6 +444,7 @@ static void iaa_crypto_remove(struct idxd_dev *idxd_dev)
 	mutex_lock(&wq->wq_lock);
 	remove_iaa_wq(wq);
 	drv_disable_wq(wq);
+	rebalance_wq_table();
 	mutex_unlock(&wq->wq_lock);
 }
 
@@ -299,6 +464,13 @@ static int __init iaa_crypto_init_module(void)
 {
 	int ret = 0;
 
+	nr_cpus = num_online_cpus();
+	nr_nodes = num_online_nodes();
+
+	wq_table = alloc_percpu(struct idxd_wq *);
+	if (!wq_table)
+		return -ENOMEM;
+
 	ret = idxd_driver_register(&iaa_crypto_driver);
 	if (ret) {
 		pr_debug("IAA wq sub-driver registration failed\n");
@@ -310,6 +482,7 @@ out:
 	return ret;
 
 err_driver_register:
+	free_percpu(wq_table);
 
 	goto out;
 }
@@ -317,6 +490,7 @@ err_driver_register:
 static void __exit iaa_crypto_cleanup_module(void)
 {
 	idxd_driver_unregister(&iaa_crypto_driver);
+	free_percpu(wq_table);
 	free_iaa_devices();
 
 	pr_debug("cleaned up\n");
