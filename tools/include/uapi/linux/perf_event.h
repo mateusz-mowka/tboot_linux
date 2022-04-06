8f352efdb3f78e1be17384425d361de99ead8953
Kan Liang <kan.liang@linux.intel.com>
2022-04-06 04:03:55 -0700

    tools headers uapi: Update tools's copy of linux/perf_event.h
    Signed-off-by: Kan Liang <kan.liang@linux.intel.com>

--- tools/include/uapi/linux/perf_event.h
+++ tools/include/uapi/linux/perf_event.h
@@ -413,7 +413,8 @@ struct perf_event_attr {
 				remove_on_exec :  1, /* event is removed from task on exec */
 				sigtrap        :  1, /* send synchronous SIGTRAP on event */
 				branch_events  :  1, /* include branch events */
-				__reserved_1   : 25;
+				reload         :  1, /* auto counter reload */
+				__reserved_1   : 24;
 
 	union {
 		__u32		wakeup_events;	  /* wakeup every n events */
@@ -1294,7 +1295,9 @@ union perf_mem_data_src {
 #define PERF_MEM_LVLNUM_L2	0x02 /* L2 */
 #define PERF_MEM_LVLNUM_L3	0x03 /* L3 */
 #define PERF_MEM_LVLNUM_L4	0x04 /* L4 */
-/* 5-0xa available */
+/* 5-0x8 available */
+#define PERF_MEM_LVLNUM_MMIO	0x09 /* MMIO */
+#define PERF_MEM_LVLNUM_HBM	0x0a /* HBM */
 #define PERF_MEM_LVLNUM_ANY_CACHE 0x0b /* Any cache */
 #define PERF_MEM_LVLNUM_LFB	0x0c /* LFB */
 #define PERF_MEM_LVLNUM_RAM	0x0d /* RAM */
