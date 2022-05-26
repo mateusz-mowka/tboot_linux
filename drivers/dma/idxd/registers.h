4846edd1547aca4cb2e524a0e85506b008492490
Dave Jiang <dave.jiang@intel.com>
2022-05-26 11:46:42 -0700

    dmaengine: idxd: add helper function for setup privileged bit
    Signed-off-by: Dave Jiang <dave.jiang@intel.com>

--- drivers/dma/idxd/registers.h
+++ drivers/dma/idxd/registers.h
@@ -358,8 +358,8 @@ union wqcfg {
 	u32 bits[8];
 } __packed;
 
-#define WQCFG_PASID_IDX                2
-#define WQCFG_PRIVL_IDX		2
+#define WQCFG_PASID_IDX		2
+#define WQCFG_PRIV_IDX		2
 #define WQCFG_OCCUP_IDX		6
 
 #define WQCFG_OCCUP_MASK	0xffff
