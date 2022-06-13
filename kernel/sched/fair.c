3d5ed74748d36d080d419d94109d666152d958ee
Ricardo Neri <ricardo.neri-calderon@linux.intel.com>
2022-06-12 20:55:38 -0700

    sched/fair: Allow active balance for misfit_task_class migrations
    This type of migration increases throughput swapping task of high class
    priority running on low-priority CPUs with low class priority tasks
    running on high-priority CPUs. Swapping tasks implies an active load
    balance.
    
    Signed-off-by: Ricardo Neri <ricardo.neri-calderon@linux.intel.com>

--- kernel/sched/fair.c
+++ kernel/sched/fair.c
@@ -9989,6 +9989,9 @@ static int need_active_balance(struct lb_env *env)
 	if (env->migration_type == migrate_misfit)
 		return 1;
 
+	if (env->migration_type == migrate_misfit_task_class)
+		return 1;
+
 	return 0;
 }
 
