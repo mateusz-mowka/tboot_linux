d6f6ed6c3982e4331b2fd8a1c4bc885b53e86e4b
Yuan Yao <yuan.yao@intel.com>
2022-06-24 17:35:29 +0800

    KVM: TDX: MMU: introduce can_yielde to tdp_mmu_alloc_sp_for_split()
    The new parameter "can_yielde" allows this function working in
    scenario which can not use per cpu slab for mmu page allocation AND
    release the MMU lock is not desired. Huge page splitting for INTEL TDX
    guest will be one use case for such scenario.
    
    No function change for exist code path.
    
    Signed-off-by: Yuan Yao <yuan.yao@intel.com>

--- arch/x86/kvm/mmu/tdp_mmu.c
+++ arch/x86/kvm/mmu/tdp_mmu.c
@@ -1103,7 +1103,7 @@ bool kvm_tdp_mmu_zap_sp(struct kvm *kvm, struct kvm_mmu_page *sp)
 
 static struct kvm_mmu_page *tdp_mmu_alloc_sp_for_split(struct kvm *kvm,
 						       struct tdp_iter *iter,
-						       bool shared);
+						       bool shared, bool can_yield);
 
 static int tdp_mmu_split_huge_page(struct kvm *kvm, struct tdp_iter *iter,
 				   struct kvm_mmu_page *sp, bool shared);
@@ -1167,7 +1167,7 @@ static bool tdp_mmu_zap_leafs(struct kvm *kvm, struct kvm_mmu_page *root,
 			if (!kvm_page_type_valid_on_level(gfn, slot, iter.level) ||
 			    (gfn & mask) < start ||
 			    end < (gfn & mask) + KVM_PAGES_PER_HPAGE(iter.level)) {
-				sp = tdp_mmu_alloc_sp_for_split(kvm, &iter, false);
+				sp = tdp_mmu_alloc_sp_for_split(kvm, &iter, false, can_yield);
 				WARN_ON(!sp);
 
 				tdp_mmu_split_huge_page(kvm, &iter, sp, false);
@@ -1825,7 +1825,8 @@ static struct kvm_mmu_page *__tdp_mmu_alloc_sp_for_split(
 
 static struct kvm_mmu_page *tdp_mmu_alloc_sp_for_split(struct kvm *kvm,
 						       struct tdp_iter *iter,
-						       bool shared)
+						       bool shared,
+						       bool can_yield)
 {
 	struct kvm_mmu_page *sp;
 	bool is_private = iter->is_private;
@@ -1842,7 +1843,7 @@ static struct kvm_mmu_page *tdp_mmu_alloc_sp_for_split(struct kvm *kvm,
 	 * allowed.
 	 */
 	sp = __tdp_mmu_alloc_sp_for_split(GFP_NOWAIT | __GFP_ACCOUNT, is_private);
-	if (sp)
+	if (sp || !can_yield)
 		return sp;
 
 	rcu_read_unlock();
@@ -1936,7 +1937,7 @@ retry:
 			continue;
 
 		if (!sp) {
-			sp = tdp_mmu_alloc_sp_for_split(kvm, &iter, shared);
+			sp = tdp_mmu_alloc_sp_for_split(kvm, &iter, shared, true);
 			if (!sp) {
 				ret = -ENOMEM;
 				trace_kvm_mmu_split_huge_page(iter.gfn,
