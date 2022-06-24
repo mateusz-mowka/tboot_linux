2256b5336a01048b93f0f84cccfd267db433e211
Jacob Pan <jacob.jun.pan@linux.intel.com>
2022-06-23 21:33:01 -0700

    dma/idxd: misc naming fixes
    Signed-off-by: Jacob Pan <jacob.jun.pan@linux.intel.com>

--- drivers/dma/idxd/cdev.c
+++ drivers/dma/idxd/cdev.c
@@ -528,7 +528,7 @@ static int idxd_user_drv_probe(struct idxd_dev *idxd_dev)
 	}
 
 	wq->type = IDXD_WQT_USER;
-	rc = drv_enable_wq(wq);
+	rc = __drv_enable_wq(wq);
 	if (rc < 0)
 		goto err;
 
@@ -543,7 +543,7 @@ static int idxd_user_drv_probe(struct idxd_dev *idxd_dev)
 	return 0;
 
 err_cdev:
-	drv_disable_wq(wq);
+	__drv_disable_wq(wq);
 err_drv_name:
 	wq->type = IDXD_WQT_NONE;
 err:
@@ -559,7 +559,7 @@ static void idxd_user_drv_remove(struct idxd_dev *idxd_dev)
 
 	mutex_lock(&wq->wq_lock);
 	idxd_wq_del_cdev(wq);
-	drv_disable_wq(wq);
+	__drv_disable_wq(wq);
 	wq->type = IDXD_WQT_NONE;
 	destroy_workqueue(wq->wq);
 	wq->wq = NULL;
