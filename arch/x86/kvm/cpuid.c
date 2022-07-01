f950715b1280f78ed984bc250e6fe8a7cb65eb4b
Jiaxi Chen <jiaxi.chen@linux.intel.com>
2022-07-01 08:55:48 -0700

    KVM: Expose new cpu features AVX_VNNI_INT8 to guest
    The bit definition:
    CPUID.(EAX=7,ECX=1):EDX[bit 4] AVX_VNNI_INT8
    
    Signed-off-by: Jiaxi Chen <jiaxi.chen@linux.intel.com>

--- arch/x86/kvm/cpuid.c
+++ arch/x86/kvm/cpuid.c
@@ -607,6 +607,9 @@ void kvm_set_cpu_caps(void)
 		F(AVX_VNNI) | F(AVX512_BF16) | F(CMPCCXADD) | F(AMX_FP16) |
 		F(AVX_IFMA));
 
+	kvm_cpu_cap_mask(CPUID_7_1_EDX,
+		F(AVX_VNNI_INT8));
+
 	kvm_cpu_cap_mask(CPUID_D_1_EAX,
 		F(XSAVEOPT) | F(XSAVEC) | F(XGETBV1) | F(XSAVES) | f_xfd
 	);
@@ -860,9 +863,9 @@ static inline int __do_cpuid_func(struct kvm_cpuid_array *array, u32 function)
 				goto out;
 
 			cpuid_entry_override(entry, CPUID_7_1_EAX);
+			cpuid_entry_override(entry, CPUID_7_1_EDX);
 			entry->ebx = 0;
 			entry->ecx = 0;
-			entry->edx = 0;
 		}
 		break;
 	case 9:
