yiliu1765/iommufd

vfio/pci: Add VFIO_DEVICE_ATTACH_IOAS
This patch supports the ioas_pagetable attachment for vfio pci devices.

An open:
Since the code are mostly common (non-pci specific), should we add the
code in vfio core instead of vfio pci? Wish to have good idea before
pushing to upstream.

Signed-off-by: Liu Yi L <yi.l.liu@intel.com>

drivers/vfio/pci/vfio_pci_core.c
@@ -658,6 +658,7 @@ int vfio_pci_core_bind_iommufd(struct vfio_device *core_vdev,
 	}
 
 	vdev->idev = idev;
+	vdev->ioas_id = IOMMUFD_INVALID_ID;
 	bind->out_devid = id;
 
 out_unlock:
@@ -673,12 +674,77 @@ void vfio_pci_core_unbind_iommufd(struct vfio_device *core_vdev)
 
 	mutex_lock(&vdev->idev_lock);
 	if (vdev->idev) {
+		if (vdev->ioas_id != IOMMUFD_INVALID_ID) {
+			vdev->iommufd = -1;
+			vdev->ioas_id = IOMMUFD_INVALID_ID;
+			iommufd_device_detach(vdev->idev);
+		}
 		iommufd_unbind_device(vdev->idev);
 		vdev->idev = NULL;
 	}
 	mutex_unlock(&vdev->idev_lock);
 }
 EXPORT_SYMBOL_GPL(vfio_pci_core_unbind_iommufd);
+
+int vfio_pci_core_attach_ioas(struct vfio_device *core_vdev,
+				struct vfio_device_attach_ioas *attach)
+{
+	struct vfio_pci_core_device *vdev =
+		container_of(core_vdev, struct vfio_pci_core_device, vdev);
+	u32 pt_id = attach->ioas_id;
+	int ret;
+
+	mutex_lock(&vdev->idev_lock);
+
+	if (!vdev->idev) {
+		ret = -EINVAL;
+		goto out_unlock;
+	}
+
+	/* Currently only allows one IOAS attach */
+	if (vdev->ioas_id != IOMMUFD_INVALID_ID) {
+		ret = -EBUSY;
+		goto out_unlock;
+	}
+
+	ret = iommufd_device_attach(vdev->idev, &pt_id);
+	if (ret)
+		goto out_unlock;
+
+	vdev->iommufd = attach->iommufd;
+	vdev->ioas_id = attach->ioas_id;
+	attach->out_hwpt_id = pt_id;
+
+out_unlock:
+	mutex_unlock(&vdev->idev_lock);
+
+	return ret;
+}
+EXPORT_SYMBOL_GPL(vfio_pci_core_attach_ioas);
+
+void vfio_pci_core_detach_ioas(struct vfio_device *core_vdev,
+				 struct vfio_device_detach_ioas *detach)
+{
+	struct vfio_pci_core_device *vdev =
+		container_of(core_vdev, struct vfio_pci_core_device, vdev);
+
+	mutex_lock(&vdev->idev_lock);
+
+	if (vdev->ioas_id == IOMMUFD_INVALID_ID)
+		goto out_unlock;
+
+	if (vdev->iommufd != detach->iommufd ||
+	    vdev->ioas_id != detach->ioas_id)
+		goto out_unlock;
+
+	vdev->iommufd = -1;
+	vdev->ioas_id = IOMMUFD_INVALID_ID;
+	iommufd_device_detach(vdev->idev);
+
+out_unlock:
+	mutex_unlock(&vdev->idev_lock);
+}
+EXPORT_SYMBOL_GPL(vfio_pci_core_detach_ioas);
 
 long vfio_pci_core_ioctl(struct vfio_device *core_vdev, unsigned int cmd,
 		unsigned long arg)

include/linux/vfio.h
@@ -66,6 +66,10 @@ struct vfio_device_ops {
 	int	(*bind_iommufd)(struct vfio_device *vdev,
 				struct vfio_device_bind_iommufd *bind);
 	void	(*unbind_iommufd)(struct vfio_device *vdev);
+	int	(*attach_ioas)(struct vfio_device *vdev,
+			       struct vfio_device_attach_ioas *attach);
+	void	(*detach_ioas)(struct vfio_device *vdev,
+			       struct vfio_device_detach_ioas *detach);
 	int	(*open_device)(struct vfio_device *vdev);
 	void	(*close_device)(struct vfio_device *vdev);
 	void	(*release)(struct vfio_device *vdev);

mateusz-mowka/tboot_linux

Signed-off-by: Yi Liu <yi.l.liu@intel.com>

drivers/vfio/pci/vfio_pci_core.c
@@ -686,7 +686,9 @@ int vfio_pci_core_bind_iommufd(struct vfio_device *core_vdev,
 		goto out_unlock;
 	}
 
+	vdev->iommufd = bind->iommufd;
 	vdev->idev = idev;
+	vdev->hwpt_id = IOMMUFD_INVALID_ID;
 	bind->out_devid = id;
 
 out_unlock:
@@ -702,12 +704,77 @@ void vfio_pci_core_unbind_iommufd(struct vfio_device *core_vdev)
 
 	mutex_lock(&vdev->idev_lock);
 	if (vdev->idev) {
+		if (vdev->hwpt_id != IOMMUFD_INVALID_ID) {
+			vdev->iommufd = -1;
+			vdev->hwpt_id = IOMMUFD_INVALID_ID;
+			iommufd_device_detach(vdev->idev);
+		}
 		iommufd_unbind_device(vdev->idev);
 		vdev->idev = NULL;
 	}
 	mutex_unlock(&vdev->idev_lock);
 }
 EXPORT_SYMBOL_GPL(vfio_pci_core_unbind_iommufd);
+
+int vfio_pci_core_attach_ioas(struct vfio_device *core_vdev,
+			      struct vfio_device_attach_ioas *attach)
+{
+	struct vfio_pci_core_device *vdev =
+		container_of(core_vdev, struct vfio_pci_core_device, vdev);
+	u32 pt_id = attach->ioas_id;
+	int ret;
+
+	mutex_lock(&vdev->idev_lock);
+
+	if (!vdev->idev || vdev->iommufd != attach->iommufd) {
+		ret = -EINVAL;
+		goto out_unlock;
+	}
+
+	/* Currently only allows one IOAS attach */
+	if (vdev->hwpt_id != IOMMUFD_INVALID_ID) {
+		ret = -EBUSY;
+		goto out_unlock;
+	}
+
+	/*
+	 * FIXME: needs to discuss the allow_unsage_interrupt. May want
+	 * to have this parameter in vfio scope.
+	 */
+	ret = iommufd_device_attach(vdev->idev, &pt_id,
+				    IOMMUFD_ATTACH_FLAGS_ALLOW_UNSAFE_INTERRUPT);
+	if (ret)
+		goto out_unlock;
+
+	vdev->hwpt_id = pt_id;
+	attach->out_hwpt_id = pt_id;
+
+out_unlock:
+	mutex_unlock(&vdev->idev_lock);
+
+	return ret;
+}
+EXPORT_SYMBOL_GPL(vfio_pci_core_attach_ioas);
+
+void vfio_pci_core_detach_hwpt(struct vfio_device *core_vdev,
+			       struct vfio_device_detach_hwpt *detach)
+{
+	struct vfio_pci_core_device *vdev =
+		container_of(core_vdev, struct vfio_pci_core_device, vdev);
+
+	mutex_lock(&vdev->idev_lock);
+
+	if (!vdev->idev ||
+	    vdev->iommufd != detach->iommufd)
+		goto out_unlock;
+
+	vdev->hwpt_id = IOMMUFD_INVALID_ID;
+	iommufd_device_detach(vdev->idev);
+
+out_unlock:
+	mutex_unlock(&vdev->idev_lock);
+}
+EXPORT_SYMBOL_GPL(vfio_pci_core_detach_hwpt);
 
 long vfio_pci_core_ioctl(struct vfio_device *core_vdev, unsigned int cmd,
 		unsigned long arg)

include/linux/vfio.h
@@ -82,6 +82,10 @@ struct vfio_device_ops {
 	int	(*bind_iommufd)(struct vfio_device *vdev,
 				struct vfio_device_bind_iommufd *bind);
 	void	(*unbind_iommufd)(struct vfio_device *vdev);
+	int	(*attach_ioas)(struct vfio_device *vdev,
+			       struct vfio_device_attach_ioas *attach);
+	void	(*detach_hwpt)(struct vfio_device *vdev,
+			       struct vfio_device_detach_hwpt *detach);
 	int	(*open_device)(struct vfio_device *vdev);
 	void	(*close_device)(struct vfio_device *vdev);
 	void	(*release)(struct vfio_device *vdev);