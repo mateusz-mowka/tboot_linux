nicolinc/iommufd

vfio/ccw: Use vfio_alloc/put_device()
Use vfio_alloc/put_device() for the vfio_ccw_private allocation, and moves
the io_mutex destroy and the free of crw_region, schib_region, cmd_region,
io_region and cp.guest_cp into the vfio_ccw_release(). As the vfio ccw uses
kfree_rcu() to solve a race, so the vfio core needs to use kfree_rcu() as
well.

Signed-off-by: Liu Yi L <yi.l.liu@intel.com>

drivers/vfio/vfio.c
@@ -504,7 +504,7 @@ static void vfio_device_release(struct kref *kref)
 	if (device->ops && device->ops->release)
 		device->ops->release(device);
 
-	kfree(device);
+	kfree_rcu(device, rcu);
 }
 
 /**

mateusz-mowka/tboot_linux

Signed-off-by: Yi Liu <yi.l.liu@intel.com>

drivers/vfio/vfio.c
@@ -488,7 +488,7 @@ static void vfio_device_release(struct kref *kref)
 	if (device->ops && device->ops->release)
 		device->ops->release(device);
 
-	kvfree(device);
+	kvfree_rcu(device, rcu);
 }
 
 /*