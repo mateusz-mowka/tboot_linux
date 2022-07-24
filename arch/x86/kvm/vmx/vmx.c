7a7421e08fcfdb6146e26eec53ce43d9a5cfe6bc
Wu Hao <hao.wu@intel.com>
2022-07-24 14:22:43 +0800

    x86: move cpu_vmxon function from kvm to common header
    As TDX module needs to be initialized early, so move this function
    to common header file.
    
    Signed-off-by: Wu Hao <hao.wu@intel.com>

--- arch/x86/kvm/vmx/vmx.c
+++ arch/x86/kvm/vmx/vmx.c
@@ -2496,22 +2496,7 @@ __init int vmx_disabled_by_bios(void)
 
 static int kvm_cpu_vmxon(u64 vmxon_pointer)
 {
-	u64 msr;
-
-	cr4_set_bits(X86_CR4_VMXE);
-
-	asm_volatile_goto("1: vmxon %[vmxon_pointer]\n\t"
-			  _ASM_EXTABLE(1b, %l[fault])
-			  : : [vmxon_pointer] "m"(vmxon_pointer)
-			  : : fault);
-	return 0;
-
-fault:
-	WARN_ONCE(1, "VMXON faulted, MSR_IA32_FEAT_CTL (0x3a) = 0x%llx\n",
-		  rdmsrl_safe(MSR_IA32_FEAT_CTL, &msr) ? 0xdeadbeef : msr);
-	cr4_clear_bits(X86_CR4_VMXE);
-
-	return -EFAULT;
+	return cpu_vmxon(vmxon_pointer);
 }
 
 int vmx_hardware_enable(void)
