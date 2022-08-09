4878a7eec106a8840dbebad770e878348728f761
Kan Liang <kan.liang@linux.intel.com>
2022-08-09 06:49:34 -0700

    x86/asm: Add WRMSRNS insns support
    Signed-off-by: Kan Liang <kan.liang@linux.intel.com>

--- arch/x86/include/asm/msr-list.h
+++ arch/x86/include/asm/msr-list.h
@@ -101,5 +101,17 @@ static __always_inline void wrmsrl_batch(unsigned int msr, u64 val)
 static __always_inline void msrlist_writel(void) {}
 
 #endif /* CONFIG_X86_64 */
+
+static inline void wrmsrns(unsigned int msr, u32 low, u32 high)
+{
+	asm volatile (".byte 0x0f,0x01,0xc6;"
+		      : : "c" (msr), "a"(low), "d" (high) : "memory");
+}
+
+static inline void wrmsrnsl(unsigned int msr, u64 val)
+{
+	wrmsrns(msr, (u32)(val & 0xffffffffULL), (u32)(val >> 32));
+}
+
 #endif /* __ASSEMBLY__ */
 #endif /* _ASM_X86_MSR_LIST_H */
