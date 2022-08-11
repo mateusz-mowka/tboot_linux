2752752858fc3745f1fd092221a6ee3e4ba90b84
Li Ming <ming4.li@intel.com>
2022-08-11 16:40:02 +0800

    x86: pci-ide: support creating IDE stream in TEE mode
    This patch is for IDE x86/intel specific implementation. TDX module
    supplies SEAMCALLs for OS to create IDE stream on RP side, and OS
    needs to initialize IDE stream on device side to enable an IDE stream
    between RP and device. Currently implementation only supports Selective
    IDE stream enablement.
    
    Signed-off-by: Xu Yilun <yilun.xu@intel.com>
    Signed-off-by: Wu Hao <hao.wu@intel.com>
    Signed-off-by: Li Ming <ming4.li@intel.com>

--- arch/x86/include/asm/pci_ide.h
+++ arch/x86/include/asm/pci_ide.h
@@ -9,4 +9,37 @@
 #define KCB_CAP_NUM_TX_KEY_SLOTS	0x000FFC00
 #define KCB_CAP_NUM_RX_KEY_SLOTS	0x3FF00000
 
+#define IDE_KM_PARAM_KSET         0x01
+#define IDE_KM_PARAM_DIR          0x02
+#define IDE_KM_PARAM_SUB_STREAM   0xF0
+
+struct stream_create_param {
+	/*
+	 * Bit 7: 0  - IDE_ID      ID of the RP IDE config. register block
+	 * Bit 15:8  - RP_DF_NUM   Device and fn. number of RP
+	 * Bit 23:16 - KEY_ID     ID of key config bar stream registers
+	 * Bit 30:24   Reserved
+	 * Bit 31      STREAM_TYPE      0: LINK_IDE, 1: SEL_IDE
+	 */
+#define STREAM_CFG_IDE_ID		0x00000000FFULL
+#define STREAM_CFG_RP_DF_NUM		0x000000FF00ULL
+#define STREAM_CFG_KEY_ID		0x0000FF0000ULL
+#define STREAM_CFG_TYPE			0x0080000000ULL
+	u64 ide_stream_cfg;
+
+	/* IDE Stream Control Register */
+	u32 ide_stream_ctrl;
+	/* IDE RID Association Register 1 */
+	u32 rid_assoc1;
+	/* IDE RID Association Register 2 */
+	u32 rid_assoc2;
+	/* IDE Address Association Register 1 */
+	u32 addr_assoc1;
+	/* IDE Address Association Register 2 */
+	u32 addr_assoc2;
+	/* IDE Address Association Register 3 */
+	u32 addr_assoc3;
+	struct tdx_td_page  stream_exinfo;
+};
+
 #endif /* _ASM_X86_PCI_IDE_H */
