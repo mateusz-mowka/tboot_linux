beda12a0ee807ecfc56aedda38e09b164b343b2f
Fenghua Yu <fenghua.yu@intel.com>
2022-09-22 16:25:26 -0700

    dmaengine: idxd: Add descriptor definitions for 16 bytes of pattern in memory fill operation
    The memory fill operation (0x04) can fill in memory with either 8 bytes
    or 16 bytes of pattern. To fill in memory with 16 bytes of pattern, the
    first 8 bytes are provided in pattern lower in bytes 16-23 and the next
    8 bytes are in pattern upper in bytes 40-47 in the descriptor. Currently
    only 8 bytes of pattern is enabled.
    
    Add descriptor definitions for pattern lower and pattern upper so that
    user can use 16 bytes of pattern to fill memory.
    
    Signed-off-by: Fenghua Yu <fenghua.yu@intel.com>
    Reviewed-by: Dave Jiang <dave.jiang@intel.com>

--- include/uapi/linux/idxd.h
+++ include/uapi/linux/idxd.h
@@ -183,6 +183,7 @@ struct dsa_hw_desc {
 		uint64_t	rdback_addr;
 		uint64_t	pattern;
 		uint64_t	desc_list_addr;
+		uint64_t	pattern_lower;
 	};
 	union {
 		uint64_t	dst_addr;
@@ -246,6 +247,9 @@ struct dsa_hw_desc {
 			uint16_t	dest_app_tag_mask;
 			uint16_t	dest_app_tag_seed;
 		};
+
+		/* Fill */
+		uint64_t	pattern_upper;
 
 		uint8_t		op_specific[24];
 	};
