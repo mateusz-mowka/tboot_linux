1dc2856724fb880e0a3223b855f0b68e732d240b
Kan Liang <kan.liang@linux.intel.com>
2022-10-24 08:14:57 -0700

    perf mem: Add a new data source MMIO
    Add a new data source MMIO, which indicates a miss in LLC serviced by
    MMIO.
    
    Reviewed-by: Andi Kleen <ak@linux.intel.com>
    Signed-off-by: Kan Liang <kan.liang@linux.intel.com>

--- tools/include/uapi/linux/perf_event.h
+++ tools/include/uapi/linux/perf_event.h
@@ -1336,7 +1336,8 @@ union perf_mem_data_src {
 #define PERF_MEM_LVLNUM_L2	0x02 /* L2 */
 #define PERF_MEM_LVLNUM_L3	0x03 /* L3 */
 #define PERF_MEM_LVLNUM_L4	0x04 /* L4 */
-/* 5-0x8 available */
+/* 5-0x7 available */
+#define PERF_MEM_LVLNUM_MMIO	0x08 /* MMIO */
 #define PERF_MEM_LVLNUM_CXL	0x09 /* CXL */
 #define PERF_MEM_LVLNUM_IO	0x0a /* I/O */
 #define PERF_MEM_LVLNUM_ANY_CACHE 0x0b /* Any cache */
