94e37248323744f4c2731e8b97a2bb0c6e11cdd1
Kan Liang <kan.liang@linux.intel.com>
2022-10-31 14:14:25 -0700

    perf/x86/intel: Support Sierra Forest
    Another CMT-based server
    
    Signed-off-by: Kan Liang <kan.liang@linux.intel.com>

--- arch/x86/events/intel/core.c
+++ arch/x86/events/intel/core.c
@@ -6533,6 +6533,7 @@ __init int intel_pmu_init(void)
 		break;
 
 	case INTEL_FAM6_ATOM_CRESTMONT_D:
+	case INTEL_FAM6_SIERRAFOREST_X:
 		x86_pmu.mid_ack = true;
 		memcpy(hw_cache_event_ids, glp_hw_cache_event_ids,
 		       sizeof(hw_cache_event_ids));
