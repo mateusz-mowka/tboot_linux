80041a29c17bf6ae3af84b9cdbe2eef5904bf620
Yian Chen <yian.chen@intel.com>
2022-11-07 11:25:35 -0800

    Revert "x86/cpu: Add command line parameter "lass""
    This reverts commit ce283fa1693e6f71ca7862884a91cad07a6c8ed6.
    
    Signed-off-by: Yian Chen <yian.chen@intel.com>

--- arch/x86/kernel/cpu/common.c
+++ arch/x86/kernel/cpu/common.c
@@ -377,21 +377,6 @@ static __always_inline void setup_smep(struct cpuinfo_x86 *c)
 		cr4_set_bits(X86_CR4_SMEP);
 }
 
-static int lass_enabled __ro_after_init;
-bool is_lass_enabled(void)
-{
-	return lass_enabled;
-}
-
-static int __init setup_enable_lass(char *arg)
-{
-	lass_enabled = cpu_feature_enabled(X86_FEATURE_LASS);
-	return 0;
-}
-
-/* apply lass before kernel maps vsyscall which is done ahead __setup() */
-early_param("lass", setup_enable_lass);
-
 #ifdef CONFIG_SVOS
 static int svos_setup_sld = 0;
 static int __init svos_enable_sld(char *str)
