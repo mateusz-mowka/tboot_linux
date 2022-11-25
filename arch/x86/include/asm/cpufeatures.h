0b421be78d025962f6d1cf306b68757b03b8964a
Chang S. Bae <chang.seok.bae@intel.com>
2022-11-25 20:58:41 +0800

    x86: KVM: Advertise AMX-FP16 CPUID to user space
    Latest Intel platform Granite Rapids has introduced a new instruction -
    AMX-FP16, which performs dot-products of two FP16 tiles and accumulates
    the results into a packed single precision tile. AMX-FP16 adds FP16
    capability and also allows a FP16 GPU trained model to run faster
    without loss of accuracy or added SW overhead.
    
    The bit definition:
    CPUID.(EAX=7,ECX=1):EAX[bit 21]
    
    AMX-FP16 is on an expected-dense CPUID leaf and some other bits on this
    leaf have kernel usages. Given that, define this feature bit like
    X86_FEATURE_<name> in kernel. Considering AMX-FP16 itself has no truly
    kernel usages and /proc/cpuinfo has too much unreadable flags, hide this
    one in /proc/cpuinfo.
    
    Advertise AMX-FP16 to KVM userspace. This is safe because there are no
    new VMX controls or additional host enabling required for guests to use
    this feature.
    
    Signed-off-by: Chang S. Bae <chang.seok.bae@intel.com>
    Signed-off-by: Jiaxi Chen <jiaxi.chen@linux.intel.com>
    Acked-by: Borislav Petkov <bp@suse.de>

--- arch/x86/include/asm/cpufeatures.h
+++ arch/x86/include/asm/cpufeatures.h
@@ -315,6 +315,7 @@
 #define X86_FEATURE_LASS		(12*32+ 6) /* Linear address space separation */
 #define X86_FEATURE_CMPCCXADD		(12*32+ 7) /* "" CMPccXADD instructions */
 #define X86_FEATURE_ARCH_PERFMON_EXT	(12*32+ 8) /* "" Intel Architectural PerfMon Extension */
+#define X86_FEATURE_AMX_FP16		(12*32+21) /* "" AMX fp16 Support */
 #define X86_FEATURE_LAM			(12*32+26) /* Linear Address Masking */
 
 /* AMD-defined CPU features, CPUID level 0x80000008 (EBX), word 13 */
