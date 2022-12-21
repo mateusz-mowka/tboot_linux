b9d9ae3ab9761314bf8c05b9f6dc245d1574a577
Ashok Raj <ashok.raj@intel.com>
2022-12-20 17:25:01 -0800

    x86/microcode/intel: Make microcode meta data common code
    The metadata parsing and some definitions are common between microcode and
    Infield Scan (IFS). Move them to cpu/intel.c  and rename them
    appropriately with intel_ prefix.
    
    Signed-off-by: Ashok Raj <ashok.raj@intel.com>

--- arch/x86/include/asm/cpu.h
+++ arch/x86/include/asm/cpu.h
@@ -96,6 +96,13 @@ extern __noendbr void cet_disable(void);
 
 struct ucode_cpu_info;
 
+#define MC_HEADER_META_TYPE_END		(0)
+
+struct metadata_header {
+	unsigned int	type;
+	unsigned int	blk_size;
+};
+
 int intel_cpu_collect_info(struct ucode_cpu_info *uci);
 
 static inline bool intel_cpu_signatures_match(unsigned int s1, unsigned int p1,
@@ -115,5 +122,6 @@ static inline bool intel_cpu_signatures_match(unsigned int s1, unsigned int p1,
 extern u64 x86_read_arch_cap_msr(void);
 int intel_find_matching_signature(void *mc, unsigned int csig, int cpf);
 int intel_microcode_sanity_check(void *mc, bool print_err, int hdr_type);
+struct metadata_header *intel_microcode_find_meta_data(void *mc, unsigned int meta_type);
 
 #endif /* _ASM_X86_CPU_H */
--- drivers/platform/x86/intel/ifs/load.c
+++ drivers/platform/x86/intel/ifs/load.c
@@ -51,38 +51,6 @@ static const char * const scan_authentication_status[] = {
 	[5] = "Interrupted"
 };
 
-#define MC_HEADER_META_TYPE_END		(0)
-
-struct metadata_header {
-	unsigned int		type;
-	unsigned int		blk_size;
-};
-
-static struct metadata_header *find_meta_data(void *ucode, unsigned int meta_type)
-{
-	struct metadata_header *meta_header;
-	unsigned long data_size, total_meta;
-	unsigned long meta_size = 0;
-
-	data_size = get_datasize(ucode);
-	total_meta = ((struct microcode_intel *)ucode)->hdr.metasize;
-	if (!total_meta)
-		return NULL;
-
-	meta_header = (ucode + MC_HEADER_SIZE + data_size) - total_meta;
-
-	while (meta_header->type != MC_HEADER_META_TYPE_END &&
-	       meta_header->blk_size &&
-	       meta_size < total_meta) {
-		meta_size += meta_header->blk_size;
-		if (meta_header->type == meta_type)
-			return meta_header;
-
-		meta_header = (void *)meta_header + meta_header->blk_size;
-	}
-	return NULL;
-}
-
 static void auth_err_message(struct device *dev, u32 err_code)
 {
 	if (err_code >= ARRAY_SIZE(scan_authentication_status))
@@ -180,7 +148,7 @@ static void chunk_mismatch_warn(struct device *dev, int total_chunks, int chunks
 {
 	union meta_data *ifs_meta;
 
-	ifs_meta = (union meta_data *)find_meta_data(ifs_header_ptr, META_TYPE_IFS);
+	ifs_meta = (union meta_data *)intel_microcode_find_meta_data(ifs_header_ptr, META_TYPE_IFS);
 
 	if (total_chunks != ifs_meta->total_chunks)
 		dev_warn(dev, "total chunks mismatch - metadata: %d MSR value: %d\n",
@@ -237,7 +205,7 @@ static int copy_hashes_authenticate_chunks_gen2(struct device *dev)
 	}
 
 	base = ifs_test_image_ptr;
-	ifs_meta = (union meta_data *)find_meta_data(ifs_header_ptr, META_TYPE_IFS);
+	ifs_meta = (union meta_data *)intel_microcode_find_meta_data(ifs_header_ptr, META_TYPE_IFS);
 	starting_chunk_nr = ifs_meta->starting_chunk;
 
 	dev_info(dev, "authenticating and copying chunk ver 0x%x , starting chunk %d\n",
@@ -383,7 +351,7 @@ static int validate_ifs_metadata(struct device *dev)
 		 boot_cpu_data.x86, boot_cpu_data.x86_model,
 		 boot_cpu_data.x86_stepping, ifsd->cur_batch);
 
-	ifs_meta = (union meta_data *)find_meta_data(ifs_header_ptr, META_TYPE_IFS);
+	ifs_meta = (union meta_data *)intel_microcode_find_meta_data(ifs_header_ptr, META_TYPE_IFS);
 	if (!ifs_meta) {
 		dev_err(dev, "IFS Metadata missing in file %s\n", test_file);
 		return ret;
