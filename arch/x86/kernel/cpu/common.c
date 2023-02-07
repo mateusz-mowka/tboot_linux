cc-api/linux

x86/cpu: Restore MSR_IA32_HW_HRESET_ENABLE MSR on BSP during wakeup
The value of MSR_IA32_HW_HRESET_ENABLE is lost when suspending. Given that
HRESET is executed when switching to kernel mode and viceversa, a general
protection fault occurs when HRESET is executed for the first time after
waking up. Thus, restore such MSR when waking up. There is no need to
restore this MSR for APs because it will be restored during CPU hotplug
online.

[Ricardo edited the commit message for clarity.]

Reported-by: Wendy Wang <wendy.wang@intel.com>
Reported-by: kernel test robot <lkp@intel.com>
Co-developed-by: Archana Patni <archana.patni@intel.com>
Signed-off-by: Chen Yu <yu.c.chen@intel.com>

arch/x86/kernel/cpu/common.c
@@ -372,6 +372,12 @@ static __always_inline void setup_hreset(struct cpuinfo_x86 *c)
 	pr_info_once("x86/cpu: Intel History Reset (HRESET) activated\n");
 }
 
+void hreset_reload(void)
+{
+	if (hreset_features)
+		wrmsrl(MSR_IA32_HW_HRESET_ENABLE, hreset_features);
+}
+
 /* These bits should not change their value after CPU init is finished. */
 static const unsigned long cr4_pinned_mask =
 	X86_CR4_SMEP | X86_CR4_SMAP | X86_CR4_UMIP | X86_CR4_FSGSBASE;

mateusz-mowka/tboot_linux

arch/x86/kernel/cpu/common.c
@@ -439,6 +439,12 @@ static __always_inline void setup_hreset(struct cpuinfo_x86 *c)
 	wrmsrl(MSR_IA32_HW_HRESET_ENABLE, hardware_history_features);
 }
 
+void hreset_reload(void)
+{
+	if (hardware_history_features)
+		wrmsrl(MSR_IA32_HW_HRESET_ENABLE, hardware_history_features);
+}
+
 /* These bits should not change their value after CPU init is finished. */
 static const unsigned long cr4_pinned_mask =
 	X86_CR4_SMEP | X86_CR4_SMAP | X86_CR4_UMIP |