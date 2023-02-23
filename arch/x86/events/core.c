11757e3359d6bd6b9aff51a6ed7aca09db6f96c7
Kan Liang <kan.liang@linux.intel.com>
2023-02-23 10:25:31 -0800

    perf/x86: Remove one active group check for LBR log event
    The NMI watchdog may always enabled in the system. The active group
    check always fails for this case.
    
    Remove the check for now. It's used to handle some corner cases. The
    removal doesn't impact the feature validation.
    
    Signed-off-by: Kan Liang <kan.liang@linux.intel.com>
    Signed-off-by: Kuppuswamy Sathyanarayanan <sathyanarayanan.kuppuswamy@linux.intel.com>

--- arch/x86/events/core.c
+++ arch/x86/events/core.c
@@ -1166,7 +1166,7 @@ static int collect_events(struct cpu_hw_events *cpuc, struct perf_event *leader,
 	int num_counters = hybrid(cpuc->pmu, num_counters);
 	int num_counters_fixed = hybrid(cpuc->pmu, num_counters_fixed);
 	struct perf_event *event;
-	int i, n, max_count;
+	int n, max_count;
 
 	max_count = num_counters + num_counters_fixed;
 
@@ -1193,16 +1193,6 @@ static int collect_events(struct cpu_hw_events *cpuc, struct perf_event *leader,
 				return -EINVAL;
 
 			cpuc->pebs_output = is_pebs_pt(leader) + 1;
-		}
-
-		/*
-		 * Only support one active group with branch events
-		 */
-		if (leader->attr.branch_events) {
-			for (i = 0; i < cpuc->n_events; i++) {
-				if (cpuc->event_list[i]->group_leader != leader->group_leader)
-					return -EINVAL;
-			}
 		}
 	}
 
