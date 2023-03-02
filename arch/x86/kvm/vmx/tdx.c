2854b3c7ef083bbc5fa0d151a56aa9189e3f217c
Chenyi Qiang <chenyi.qiang@intel.com>
2023-03-02 03:33:11 +0000

    Revert "KVM: TDX: fix the missing wait description page allocation"
    This reverts commit c2e66e5229ccafe36e63415ded74ef8862b54e03.

--- arch/x86/kvm/vmx/tdx.c
+++ arch/x86/kvm/vmx/tdx.c
@@ -5103,10 +5103,6 @@ static struct tdx_iommu *tdx_iommu_get(u64 iommu_id)
 	}
 
 	tiommu->iommu_id = iommu_id;
-	raw_spin_lock_init(&tiommu->invq_lock);
-	ret = tdx_alloc_td_page(&tiommu->wait_desc);
-	if (ret)
-		goto free_tiommu;
 
 	kref_init(&tiommu->ref);
 
@@ -5115,8 +5111,6 @@ static struct tdx_iommu *tdx_iommu_get(u64 iommu_id)
 
 	return tiommu;
 
-free_tiommu:
-	kfree(tiommu);
 unlock:
 	mutex_unlock(&global_tiommu_lock);
 	return ERR_PTR(ret);
@@ -5130,7 +5124,6 @@ static void tdx_iommu_release(struct kref *kref)
 	list_del(&tiommu->node);
 	mutex_unlock(&global_tiommu_lock);
 
-	tdx_reclaim_td_page(&tiommu->wait_desc);
 	kfree(tiommu);
 }
 
