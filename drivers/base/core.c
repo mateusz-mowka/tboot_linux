d4f7fe16081bd9e07a6482a402022fca5122c22c
Chenyi Qiang <chenyi.qiang@intel.com>
2023-03-02 03:05:14 +0000

    Revert "Revert "ioremap: redefine the authorized field for devices""
    This reverts commit cea553a69b04bec697c444734fbda4a0ee242d5a.

--- drivers/base/core.c
+++ drivers/base/core.c
@@ -53,7 +53,7 @@ early_param("sysfs.deprecated", sysfs_deprecated_setup);
  * Default authorization status set as allow all. It can be
  * overridden by arch code.
  */
-int __ro_after_init dev_default_authorization = MODE_SHARED;
+int __ro_after_init dev_default_authorization = MODE_AUTHORIZED;
 
 /* Device links support. */
 static LIST_HEAD(deferred_sync);
