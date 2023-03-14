bad428f0dffb381c38fd6911066e3f9a179636f2
Qiuxu Zhuo <qiuxu.zhuo@intel.com>
2023-03-14 11:05:29 +0800

    x86/microcode: Fix build error "too few arguments to request_microcode_fw()"
    When building kernel, it got the build error message:
    
      arch/x86/kernel/cpu/microcode/core.c:695:12: error:
      too few arguments to function ‘microcode_ops->request_microcode_fw’
      tmp_ret = microcode_ops->request_microcode_fw(bsp, &microcode_pdev->dev);
    
    Fix this build error by passing the missed 'type' parameter to
    microcode_ops->request_microcode_fw().
    
    Signed-off-by: Qiuxu Zhuo <qiuxu.zhuo@intel.com>

--- arch/x86/kernel/cpu/microcode/core.c
+++ arch/x86/kernel/cpu/microcode/core.c
@@ -690,7 +690,7 @@ static ssize_t reload_store_common(struct device *dev,
 	if (val != 1)
 		return size;
 
-	tmp_ret = microcode_ops->request_microcode_fw(bsp, &microcode_pdev->dev);
+	tmp_ret = microcode_ops->request_microcode_fw(bsp, &microcode_pdev->dev, type);
 	if (tmp_ret != UCODE_NEW) {
 		if (tmp_ret == UCODE_ERROR)
 			return -EINVAL;
