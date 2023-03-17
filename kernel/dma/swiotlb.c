cc-api/linux

swiotlb: fast shared memory allocation
Page conversion from private to shared is slow. TD guest needs to evict
cachelines with the old attribute. Our experiments show that it takes ~2
seconds to flush 1GB memory with clflush. This slows TD guest boot-time
significantly.

Allocate unaccepted memory, skip the clflush (and TLB flush) when
converting it to shared.

Note that for TDX, secure-EPT mapping for a page is setup on page accept.
So, for unaccepted memory, secure-EPT mapping isn't present. then,
converting unaccepted pages to shared doesn't need flush cache and TLB.

Signed-off-by: Chao Gao <chao.gao@intel.com>

kernel/dma/swiotlb.c
@@ -69,6 +69,8 @@ static bool swiotlb_force_disable;
 
 struct io_tlb_mem io_tlb_default_mem;
 
+static bool default_mem_unaccepted;
+
 phys_addr_t swiotlb_unencrypted_base;
 
 static unsigned long default_nslabs = IO_TLB_DEFAULT_SIZE >> IO_TLB_SHIFT;
@@ -261,7 +263,10 @@ void __init swiotlb_update_mem_attributes(void)
 		return;
 	vaddr = phys_to_virt(mem->start);
 	bytes = PAGE_ALIGN(mem->nslabs << IO_TLB_SHIFT);
-	set_memory_decrypted((unsigned long)vaddr, bytes >> PAGE_SHIFT);
+	if (default_mem_unaccepted)
+		set_memory_decrypted_noflush((unsigned long)vaddr, bytes >> PAGE_SHIFT);
+	else
+		set_memory_decrypted((unsigned long)vaddr, bytes >> PAGE_SHIFT);
 
 	mem->vaddr = swiotlb_mem_remap(mem, bytes);
 	if (!mem->vaddr)
@@ -302,7 +307,6 @@ static void swiotlb_init_io_tlb_mem(struct io_tlb_mem *mem, phys_addr_t start,
 	if (swiotlb_unencrypted_base)
 		return;
 
-	memset(vaddr, 0, bytes);
 	mem->vaddr = vaddr;
 	return;
 }
@@ -318,7 +322,7 @@ void __init swiotlb_init_remap(bool addressing_limit, unsigned int flags,
 	unsigned long nslabs = default_nslabs;
 	size_t alloc_size;
 	size_t bytes;
-	void *tlb;
+	void *tlb = NULL;
 
 	if (!addressing_limit && !swiotlb_force_bounce)
 		return;
@@ -338,10 +342,17 @@ void __init swiotlb_init_remap(bool addressing_limit, unsigned int flags,
 	 */
 retry:
 	bytes = PAGE_ALIGN(nslabs << IO_TLB_SHIFT);
-	if (flags & SWIOTLB_ANY)
-		tlb = memblock_alloc(bytes, PAGE_SIZE);
-	else
-		tlb = memblock_alloc_low(bytes, PAGE_SIZE);
+	if (cc_platform_has(CC_ATTR_GUEST_MEM_ENCRYPT)) {
+		tlb = memblock_alloc_raw_unaccepted(bytes, PAGE_SIZE);
+		if (tlb)
+			default_mem_unaccepted = true;
+	}
+	if (!tlb) {
+		if (flags & SWIOTLB_ANY)
+			tlb = memblock_alloc(bytes, PAGE_SIZE);
+		else
+			tlb = memblock_alloc_low(bytes, PAGE_SIZE);
+	}
 	if (!tlb) {
 		if (cc_platform_has(CC_ATTR_GUEST_MEM_ENCRYPT))
 			panic("%s: failed to allocate tlb structure", __func__);

mateusz-mowka/tboot_linux

kernel/dma/swiotlb.c
@@ -73,6 +73,8 @@ static bool swiotlb_force_disable;
 
 struct io_tlb_mem io_tlb_default_mem;
 
+static bool default_mem_unaccepted;
+
 phys_addr_t swiotlb_unencrypted_base;
 
 static unsigned long default_nslabs = IO_TLB_DEFAULT_SIZE >> IO_TLB_SHIFT;
@@ -281,7 +283,10 @@ void __init swiotlb_update_mem_attributes(void)
 		return;
 	vaddr = phys_to_virt(mem->start);
 	bytes = PAGE_ALIGN(mem->nslabs << IO_TLB_SHIFT);
-	set_memory_decrypted((unsigned long)vaddr, bytes >> PAGE_SHIFT);
+	if (default_mem_unaccepted)
+		set_memory_decrypted_noflush((unsigned long)vaddr, bytes >> PAGE_SHIFT);
+	else
+		set_memory_decrypted((unsigned long)vaddr, bytes >> PAGE_SHIFT);
 
 	mem->vaddr = swiotlb_mem_remap(mem, bytes);
 	if (!mem->vaddr)
@@ -328,7 +333,6 @@ static void swiotlb_init_io_tlb_mem(struct io_tlb_mem *mem, phys_addr_t start,
 	if (swiotlb_unencrypted_base)
 		return;
 
-	memset(vaddr, 0, bytes);
 	mem->vaddr = vaddr;
 	return;
 }
@@ -337,17 +341,24 @@ static void *swiotlb_memblock_alloc(unsigned long nslabs, unsigned int flags,
 		int (*remap)(void *tlb, unsigned long nslabs))
 {
 	size_t bytes = PAGE_ALIGN(nslabs << IO_TLB_SHIFT);
-	void *tlb;
+	void *tlb = NULL;
 
 	/*
 	 * By default allocate the bounce buffer memory from low memory, but
 	 * allow to pick a location everywhere for hypervisors with guest
 	 * memory encryption.
 	 */
-	if (flags & SWIOTLB_ANY)
-		tlb = memblock_alloc(bytes, PAGE_SIZE);
-	else
-		tlb = memblock_alloc_low(bytes, PAGE_SIZE);
+	if (cc_platform_has(CC_ATTR_GUEST_MEM_ENCRYPT)) {
+		tlb = memblock_alloc_raw_unaccepted(bytes, PMD_SIZE);
+		if (tlb)
+			default_mem_unaccepted = true;
+	}
+	if (!tlb) {
+		if (flags & SWIOTLB_ANY)
+			tlb = memblock_alloc(bytes, PAGE_SIZE);
+		else
+			tlb = memblock_alloc_low(bytes, PAGE_SIZE);
+	}
 
 	if (!tlb) {
 		pr_warn("%s: Failed to allocate %zu bytes tlb structure\n",