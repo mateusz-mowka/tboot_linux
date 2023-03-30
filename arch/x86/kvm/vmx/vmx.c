422e49908601f80b48e1062ba7244a28538db683
Isaku Yamahata <isaku.yamahata@intel.com>
2023-03-30 22:10:58 +1300

    KVM: X86: Move kvm_cpu_vmxon() out of KVM
    TDX host kernel support needs to enable VMX outside of KVM.  Move
    kvm_cpu_vmxon() from KVM's vmx.c to asm/virtext.h so the non-KVM code
    can enable VMX too.
    
    Also rename kvm_cpu_vmxon() to cpu_vmxon() because it is no longer KVM
    only.  As a bonus, it matches more with cpu_vmxoff() which is already in
    virtext.h.
    
    No functional change intended.
    
    Signed-off-by: Isaku Yamahata <isaku.yamahata@intel.com>
    Signed-off-by: Kai Huang <kai.huang@intel.com>

--- arch/x86/kvm/vmx/vmx.c
+++ arch/x86/kvm/vmx/vmx.c
@@ -2877,26 +2877,6 @@ int vmx_check_processor_compat(void)
 	return 0;
 }
 
-static int kvm_cpu_vmxon(u64 vmxon_pointer)
-{
-	u64 msr;
-
-	cr4_set_bits(X86_CR4_VMXE);
-
-	asm_volatile_goto("1: vmxon %[vmxon_pointer]\n\t"
-			  _ASM_EXTABLE(1b, %l[fault])
-			  : : [vmxon_pointer] "m"(vmxon_pointer)
-			  : : fault);
-	return 0;
-
-fault:
-	WARN_ONCE(1, "VMXON faulted, MSR_IA32_FEAT_CTL (0x3a) = 0x%llx\n",
-		  rdmsrl_safe(MSR_IA32_FEAT_CTL, &msr) ? 0xdeadbeef : msr);
-	cr4_clear_bits(X86_CR4_VMXE);
-
-	return -EFAULT;
-}
-
 int vmx_hardware_enable(void)
 {
 	int cpu = raw_smp_processor_id();
@@ -2916,7 +2896,7 @@ int vmx_hardware_enable(void)
 
 	intel_pt_handle_vmx(1);
 
-	r = kvm_cpu_vmxon(phys_addr);
+	r = cpu_vmxon(phys_addr);
 	if (r) {
 		intel_pt_handle_vmx(0);
 		return r;
