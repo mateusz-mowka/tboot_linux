e73cce6d851e95cb902e33d466b5c0a7ee216ed2
Yao Yuan <yuan.yao@intel.com>
2023-04-13 15:59:59 +0800

    coco/tdx: [REVERT][WR-LFE-7516] Ignore SEPT_VE_DISABLE bit checking
    pr_err() instead of panic() when the bit is not set, it's not
    supported in TDX module 2.0.22/24, more details:
    
    https://jira.devtools.intel.com/browse/LFE-7516
    
    Signed-off-by: Yao Yuan <yuan.yao@intel.com>

--- arch/x86/coco/tdx/tdx.c
+++ arch/x86/coco/tdx/tdx.c
@@ -300,7 +300,7 @@ static void tdx_parse_tdinfo(u64 *cc_mask)
 	 */
 	td_attr = out.rdx;
 	if (!(td_attr & ATTR_SEPT_VE_DISABLE))
-		panic("TD misconfiguration: SEPT_VE_DISABLE attibute must be set.\n");
+		pr_err("workaround for LFE-7516: Ignore SEPT_VE_DISABLE attibute not set, it's not supported by TDX module 2.0.22/24\n");
 }
 
 /*
