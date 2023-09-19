cc-api/linux

KVM: TDX: Fix page merging TDH_MEM_PAGE_PROMOTE failures
TDH_MEM_PAGE_PROMOTE has `operand ID` in bits[31:0], add
tdx_masked_status to mask the lower operand ID bits to
handle the accepted/pending page failures.

Signed-off-by: Xiaocheng Dong <xiaocheng.dong@intel.com>

arch/x86/kvm/vmx/tdx.c
@@ -2561,7 +2561,7 @@ static int tdx_sept_merge_private_spt(struct kvm *kvm, gfn_t gfn,
 
 	/* See comment in tdx_sept_set_private_spte() */
 	err = tdh_mem_page_promote(kvm_tdx->tdr_pa, gpa, tdx_level, &out);
-	if (err == TDX_EPT_INVALID_PROMOTE_CONDITIONS)
+	if (tdx_masked_status(err) == TDX_EPT_INVALID_PROMOTE_CONDITIONS)
 		/*
 		 * Some pages are accepted, some pending.  Need to wait for TD
 		 * to accept all pages.  Tell it the caller.

mateusz-mowka/tboot_linux

arch/x86/kvm/vmx/tdx.c
@@ -2610,7 +2610,7 @@ static int tdx_sept_merge_private_spt(struct kvm *kvm, gfn_t gfn,
 
 	/* See comment in tdx_sept_set_private_spte() */
 	err = tdh_mem_page_promote(kvm_tdx->tdr_pa, gpa, tdx_level, &out);
-	if (err == TDX_EPT_INVALID_PROMOTE_CONDITIONS)
+	if (seamcall_masked_status(err) == TDX_EPT_INVALID_PROMOTE_CONDITIONS)
 		/*
 		 * Some pages are accepted, some pending.  Need to wait for TD
 		 * to accept all pages.  Tell it the caller.